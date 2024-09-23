8bc9f8982998b0e2872c488ef64eb9d75bf02f8d
Rrrinav <rinavhansa4@gmail.com>
2024-09-23 17:45:52 +0530

    [ Support for custom classes ]: Created template create object function to return pointer to that class object in arena using placement new!

--- r_ena/main.cpp
+++ r_ena/main.cpp
@@ -1,34 +1,66 @@
 #include <cstddef>
+#include <cstring>
 #include <iostream>
 
+// Arena allocator
 namespace utl
 {
-  class r_ena
+  class R_ena
   {
-    std::size_t size;
-    std::size_t offset;
-    char *_arena;
+    std::size_t size;    // > Size of the arena
+    std::size_t offset;  // > Offset to the next allocation
+    char *_arena;        // > Pointer to the beginning of arena
 
   public:
-    r_ena(std::size_t size = 1024) : size(size), offset(0) { _arena = new char[size]; }
+    R_ena(std::size_t size = 1024) : size(size), offset(0) { _arena = new char[size]; }
 
-    ~r_ena() { delete[] _arena; }
+    ~R_ena() { delete[] _arena; }
 
-    void *allocate(std::size_t bytes)
+    void *allocate_raw(std::size_t bytes)
     {
+      // Align the sized based on architecture using [ sizeof(void *) ]
       std::size_t alignedSize = (bytes + sizeof(void *) - 1) & ~(sizeof(void *) - 1);
 
+      // Check if we have enough memory
       if (offset + alignedSize > size)
+      {
+        std::cerr << "[ ERROR ]: Not enough memory, returning a nullptr\n";
         return nullptr;  // Not enough memory
-
+      }
+      // Return the pointer to the memory and increment the offset
       void *ptr = _arena + offset;
       offset += alignedSize;
       return ptr;
     }
-
     void reset()
     {
-      offset = 0;  // Reset the offset for reuse
+      offset = 0;
+      memset(_arena, 0, size);
+    }
+
+    void deallocate_whole()
+    {
+      offset = 0;
+      delete[] _arena;
+    }
+
+    template <typename T, typename... Targs>
+    T *create_object(Targs &&...Fargs)
+    {
+      void *memory = allocate_raw(sizeof(T));
+      if (!memory)
+      {
+        std::cerr << "[ ERROR ]: Some problem allocating memory, returning a nullptr\n";
+        return nullptr;
+      }
+      return new (memory) T(std::forward<Targs>(Fargs)...);
+    }
+
+    template <typename T>
+    void destroy_object(T *ptr)
+    {
+      if (ptr)
+        ptr->~T();  // Explicitly call destructor
     }
   };
 }  // namespace utl
@@ -38,27 +70,21 @@ class MyClass
 public:
   int value;
 
-  MyClass(int val) : value(val) { std::cout << "MyClass constructed with value: " << value << std::endl; }
+  MyClass(int val) : value(val) { std::cerr << "MyClass constructed with value: " << value << std::endl; }
 
-  ~MyClass() { std::cout << "MyClass destructed with value: " << value << std::endl; }
+  ~MyClass() { std::cerr << "MyClass destructed with value: " << value << std::endl; }
 };
 
 int main()
 {
-  utl::r_ena arena(1024);  // Create an arena with 1024 bytes
+  utl::R_ena arena(1024);  // Create an arena with 1024 bytes
 
   // Allocate memory for an instance of MyClass
-  MyClass *myObject = static_cast<MyClass *>(arena.allocate(sizeof(MyClass)));
+  MyClass *myObject = arena.create_object<MyClass>(42);
   if (myObject)
   {
-    // Construct the object using placement new
-    new (myObject) MyClass(42);
-
     // Use the object
     std::cout << "MyClass value: " << myObject->value << std::endl;
-
-    // Manually call the destructor
-    myObject->~MyClass();
   }
   else
   {
